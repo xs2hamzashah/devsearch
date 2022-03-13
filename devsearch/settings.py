2e92073f67f85f25a2fca1510aaf30c0a148c09b
HamzaShah <xs2hamzashah@gmail.com]>
2022-03-13 21:09:25 +0500

    database change to mysql

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : 'devsearch',     
+        'USER'    : 'root',          
+        'PASSWORD': 'root',          
+        'HOST'    : 'localhost',               
+        'PORT'    : '3306',
     }
 }
 
