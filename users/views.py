4aac40ab4e4f7a57d949f4386ec44408969cd2fa
HamzaShah <xs2hamzashah@gmail.com]>
2022-03-14 12:16:36 +0500

    user-profile added and skill model added with profie and page styled

--- users/views.py
+++ users/views.py
@@ -4,5 +4,17 @@ from .models import Profile
 
 # Create your views here.
 def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
     
-    return render(request, 'users/profiles.html')
+    return render(request, 'users/profiles.html', context)
+
+
+def userProfile(requset, pk):
+    profile = Profile.objects.get(pk=pk)
+    topSkills = profile.skill_set.exclude(description__exact='')
+    otherSkills = profile.skill_set.filter(description='')
+   
+    context = {'profile':profile, 'topSkills':topSkills, 'otherSkills':otherSkills}
+    return render(requset, 'users/user-profile.html', context)
+
