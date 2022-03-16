6d754f2fa8c60c7f29fd308f8e3f8004039c88dc
xs2hamzashah <xs2hamzashah@gmail.com>
2022-03-16 19:50:25 +0500

    user project CRUD added .. now user can add edit delete project

--- projects/views.py
+++ projects/views.py
@@ -22,12 +22,15 @@ def project(request, pk):
 
 @login_required(login_url='login')
 def create_project(request):
+    profile = request.user.profile
     form = ProjectForm()
 
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
             return redirect('projects')
 
     context = {'form': form}
@@ -35,8 +38,10 @@ def create_project(request):
 
 
 @login_required(login_url='login')
-def update_project(request, pk): 
-    project = Project.objects.get(id=pk)
+def update_project(request, pk):
+    profile = request.user.profile
+
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
 
     if request.method == 'POST':
@@ -51,7 +56,8 @@ def update_project(request, pk):
 
 @login_required(login_url='login')
 def delete_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
 
     if request.method == 'POST':
         project.delete()
