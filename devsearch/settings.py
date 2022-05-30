ab944cb4324d6da070a3702dcd6d14be83b3882b
xs2hamzashah <xs2hamzashah@gmail.com>
2022-05-30 09:56:39 +0500

    forgot passsword functionality added.

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -124,6 +124,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'enter2hamza@gmail.com'
+EMAIL_HOST_PASSWORD = 'cbxolqhnzebzjrid'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
+
+from devsearch.settings import EMAIL_HOST_USER
 from .models import Profile
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+
+from django.core.mail import send_mail
+from django.conf import settings
 
 # @receiver(post_save,sender=Profile)
 def createProfile(sender, instance, created , **kwargs):
@@ -11,6 +16,17 @@ def createProfile(sender, instance, created , **kwargs):
     if created:
         user = instance 
         profile = Profile.objects.create(user=user, username=user.username,email=user.email, name=f"{user.first_name} {user.last_name}")
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!'
+        
+        send_mail(
+            subject,
+            message, 
+            settings.EMAIL_HOST_USER,
+            [profile.email], 
+            fail_silently=False
+        )
     
 
 def updateUser(sender, instance, created , **kwargs):
